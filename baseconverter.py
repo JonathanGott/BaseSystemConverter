cbd93843ff0f70010b3a3dd650c12d510b741fb9
Charlie <charlieedoherty@gmail.com>
2024-04-11 11:31:06 -0500

    Add user interactivity and create non-implemented base_converter function

---
+++ baseconverter.py
@@ -0,0 +1,6 @@
+def base_converter(in_base, in_number, out_base):
+    """Takes the input base, input number, and output base; returns the output number"""
+
+    # Math will be done here to convert
+    out_number = in_number
+    return out_number
---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Created by: Charlie Doherty and Jonathon Gott
+HackKU - University of Kansas
+4/11/2024
+
+Run the main function in this file to begin the program
+"""
+from baseconverter import base_converter
+
+if __name__ == "__main__":
+
+    print("Hello, welcome to our base conversion project for HackKU")
+    # Use input to force an input to continue
+    input("Press 'enter' to continue...\n")
+
+    in_base = 10
+    in_number = int(input("What number would you like to translate: "))
+    out_base = int(input(f"What base would you like to convert {in_number} to: "))
+
+    out_number = base_converter(in_base, in_number, out_base)
+
+    print(f"Base {in_base}: {in_number}\n"
+          f"Base {out_base}: {out_number}")
